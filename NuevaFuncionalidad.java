ad0ec8590f8fc7df8b665d46b0925bf99f6f0dc7
yasmine <yasmine.essalhii@gmail.com>
2026-02-23 22:54:44 +0100

    Create NuevaFuncionalidad.java
    Hola esta es mi nueva funcionalidad implementada

---
+++ NuevaFuncionalidad.java
@@ -0,0 +1,10 @@
+package git;
+
+public class NuevaFuncionalidad {
+
+	public static void main(String[] args) {
+		System.out.println("Hola a todos esta es mi nueva funcioncionalidad implementada");
+
+	}
+
+}
